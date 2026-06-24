9cae6467ebe4b2efafba79a028837e2823bdb529
Ben Krueger <benjkrueger@gmail.com>
2026-06-24 08:51:21 -0500

    Finished JS30 Day 7, Added Day 8

--- js30/06 - Type Ahead/script.js
+++ js30/06 - Type Ahead/script.js
@@ -1,1 +1,51 @@
 const endpoint = 'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json';
+
+const cities = [];
+
+fetch(endpoint)
+    .then(blob => blob.json())
+    .then(data => cities.push(...data))
+
+console.log(cities)
+    
+function findMatches(wordToMatch, cities) {
+    return cities.filter(place => {
+        /* const city_lower = place.city.toLowerCase()
+        const state_lower = place.state.toLowerCase()
+        return city_lower.includes(wordToMatch.toLowerCase()) || state_lower.includes(wordToMatch.toLowerCase())*/
+        const regex = new RegExp(wordToMatch, 'gi')
+        return place.city.match(regex) || place.state.match(regex)
+    })
+}
+
+function numberWithCommas(x) {
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')
+}
+
+function displayMatches() {
+    console.log(this.value, this.value === "")
+    if (this.value === "") {
+        suggestions.innerHTML = `
+        <li>Filter for a city</li>
+        <li>or a state</li>`
+        return
+    }
+    const matchArray = findMatches(this.value, cities)
+    const html = matchArray.map(place => {
+        const regex = new RegExp(this.value, 'gi')
+        const cityName = place.city.replace(regex, `<span class="hl">${this.value}</span>`)
+        const stateName = place.state.replace(regex, `<span class="hl">${this.value}</span>`)
+        return `
+        <li>
+            <span class="name">${cityName}, ${stateName}</span>
+            <span class="population">${numberWithCommas(place.population)}</span>
+        </li>`
+    }).join('')
+    console.log(html)
+    suggestions.innerHTML = html;
+}
+
+const searchInput = document.querySelector('.search');
+const suggestions = document.querySelector('.suggestions')
+searchInput.addEventListener('change', displayMatches)
+searchInput.addEventListener('keyup', displayMatches)
